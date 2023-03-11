5060c344af81281d6bbbd3a619b5243b2d9544e3
wenchijchang <heyyhey@hotmail.com>
2023-03-11 17:54:57 -0500

    added ERD + updated serializers, urls, and views files in comments app + migrated comments and replies to SQL

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_comments),
-    path('<str:video_id>/', views.video_comments)
+    path('<str:video_id>/', views.video_comments),
+    path('<str:user_id>/', views.user_comment)
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import CommentSerializer
 from . models import Comment
 from replies.serializers import ReplySerializer
@@ -9,6 +10,7 @@ from replies.models import Reply
 
 # Create your views here.
 @api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
 def all_comments(request):
     if request.method == 'GET':
         all_comments = Comment.objects.all()
@@ -20,9 +22,27 @@ def all_comments(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 @api_view(['GET'])
-def video_comments( video_id):
-    comments = Comment.objects.filter(video_id=video_id)
+@permission_classes([AllowAny])
+def video_comments(request):
+    comments = Comment.objects.filter(video_id=request.video_id)
     serializer = CommentSerializer(comments, many=True)
     return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def user_comment(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        comments = Comment.objects.filter(user_id=request.user.id)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
